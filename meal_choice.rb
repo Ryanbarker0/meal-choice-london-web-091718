a744b64726fe2351d462d3ae1cbb92bd25608453
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-07 10:14:46 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice(preference)
-  puts "#{preference}"
-  binding.pry
+def meal_choice(name)
+  puts "#{name}"
 end
